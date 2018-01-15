46700cd5351e4fb5a93d5d588e13c5fdd172d58f
Seamus Connolly <sghconnolly@gmail.com>
2018-01-15 15:51:32 +0000

    Completed star rating functionality and reset

--- js/app.js
+++ js/app.js
@@ -21,6 +21,24 @@ const reset = document.getElementById('reset');
 // Function declarations *************************************************************************
 
 
+// Star rating function
+function starRating(moveCount) {
+		console.log('In the starRating function. moveCount = ' + moveCount);
+		if(moveCount === 12) {
+			let star = document.getElementById('star3');
+			star.className = "fa fa-star-o";
+			console.log(star.className);
+		} else if(moveCount === 18) {
+			let star = document.getElementById('star2');
+			star.className = "fa fa-star-o";
+		} else if(moveCount >= 24) {
+			let star = document.getElementById('star1');
+			star.className = "fa fa-star-o";
+		}
+		return;
+}
+
+
 // Function to check selected cards for a match
 function matchCards(card) {
     // Change card to open state to show that it has been selected
@@ -42,26 +60,28 @@ function matchCards(card) {
 			card1Address.setAttribute('class', 'card match');
 			card2Address.setAttribute('class', 'card match');
 			// Reset the card match array for the next selected pair of cards
-				cardsMatchCheck = [];
-				moveCount++;
-				countDisplay.innerHTML = moveCount;
-				cardsMatchedCount += 2;
+			cardsMatchCheck = [];
+			moveCount++;
+			starRating(moveCount);
+			countDisplay.innerHTML = moveCount;
+			cardsMatchedCount += 2;
 			// Announce the win
 			if (cardsMatchedCount === 16) {
 				setTimeout(function() {
-					alert('Congratulations, you win!'); // for manual testing only *****************************************
+					alert('Congratulations, you win!'); // for manual testing only. NEEDS TO BE BUILD FOR FINAL VERSION *****************************************
 				}, 1000);
 			}
 			return;
 		} else {
-			// Wait for this time (1 sec) before non-matching cards are turned back over
+			// Wait for this time (0.5s) before non-matching cards are turned back over
 			setTimeout(function() {
 				card1Address.setAttribute('class', 'card');
 				card2Address.setAttribute('class', 'card');
-			}, 1000);
+			}, 500);
 			// Reset the card match array for the next selected pair of cards
 			cardsMatchCheck = [];
 			moveCount++;
+			starRating(moveCount);
 			countDisplay.innerHTML = moveCount;
 			return;
 		}
@@ -81,11 +101,11 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
 
 
+// Start of game (set up) function
 function startGame (availableCards) {
 	// Initialise symbol deck
 	let symbolDeck = ['fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-cube', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-bomb', 'fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-cube', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-bomb'];
@@ -111,21 +131,31 @@ function startGame (availableCards) {
 }
 
 
-// Programme code ********************************************************************************
+// ***********************************************************************************************
+// *************************************** Programme code ****************************************
+// ***********************************************************************************************
+
 
 // Gather cards from deck for array assignment
 availableCards = document.getElementsByClassName('card');
 
+// Function call to start the game
 startGame(availableCards);
 
-
+// Event Listener for reset button
 reset.addEventListener('click', function reset() {
-	console.log('Reset clicked');
+	// Reset variables
 	moveCount = 0;
 	countDisplay.innerHTML = 0;
 	cardsMatchedCount = 0;
+	// Turn all cards face down
 	for (let i = 0; i < availableCards.length; i++) {
 		availableCards[i].setAttribute('class', 'card');
 	}
+	// Rest the rating stars to full
+	document.getElementById('star1').className = "fa fa-star";
+	document.getElementById('star2').className = "fa fa-star";
+	document.getElementById('star3').className = "fa fa-star";
+	// Function call to start the game - initialise
 	startGame(availableCards);
 });
